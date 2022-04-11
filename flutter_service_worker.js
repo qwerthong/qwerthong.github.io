c11d89fb567b8c09e7e7e5422f9370f2e5b25892
qwerthong <100760200+qwerthong@users.noreply.github.com>
2022-04-11 11:20:19 +0900

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "acb78206611f60d54d19242d31402e95",
-"index.html": "085f79246cc416aa2fb17d51806f0603",
-"/": "085f79246cc416aa2fb17d51806f0603",
+"index.html": "88d809455209434203c71d0df321294c",
+"/": "88d809455209434203c71d0df321294c",
 "main.dart.js": "98c837f9416c0570565cbe7a404dcb54",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
